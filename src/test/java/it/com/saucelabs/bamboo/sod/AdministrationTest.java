380156a133353e98d7e6c189eb82603457df7ef8
Ross Rowe <piaross@gmail.com>
2011-11-29 20:44:42 +1100

    Further work on integration tests

--- src/test/java/it/com/saucelabs/bamboo/sod/AdministrationTest.java
+++ src/test/java/it/com/saucelabs/bamboo/sod/AdministrationTest.java
@@ -3,43 +3,37 @@ package it.com.saucelabs.bamboo.sod;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import static org.junit.Assert.assertTrue;
 
 
 public class AdministrationTest extends IntegrationTeztHelper {
 
-    
-    @Test    
+
+    @Test
     public void testAdmin() throws Exception {
-        selenium.open("/allPlans.action");
-        selenium.waitForPageToLoad("30000");
-        selenium.click("admin");
-        selenium.waitForPageToLoad("30000");
-        selenium.click("sauceOnDemandConfig");
-        selenium.waitForPageToLoad("30000");
-        assertTrue(selenium.isElementPresent("sauceOnDemandConfigurationForm_username"));
-        assertTrue(selenium.isElementPresent("sauceOnDemandConfigurationForm_accesskey"));
-        assertTrue(selenium.isElementPresent("sauceOnDemandConfigurationForm_seleniumHost"));
-        assertTrue(selenium.isElementPresent("sauceOnDemandConfigurationForm_seleniumPort"));
-        selenium.type("sauceOnDemandConfigurationForm_username", "admin");
-        selenium.type("sauceOnDemandConfigurationForm_accesskey", "admin");
-        selenium.type("sauceOnDemandConfigurationForm_seleniumHost", "saucelabs.com");
-        selenium.type("sauceOnDemandConfigurationForm_seleniumPort", "4445");
-        selenium.click("sauceOnDemandConfigurationForm_save");
-        selenium.waitForPageToLoad("30000");
+        selenium.get("start.action");
+        selenium.findElement(By.linkText("log in")).click();
+        selenium.findElement(By.id("loginForm_os_username")).clear();
+        selenium.findElement(By.id("loginForm_os_username")).sendKeys("admin");
+        selenium.findElement(By.id("loginForm_save")).click();
+        selenium.findElement(By.id("admin")).click();
+        selenium.findElement(By.id("sauceOnDemandConfig")).click();
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_username")).clear();
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_username")).sendKeys("admin");
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_accessKey")).clear();
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_accessKey")).sendKeys("admin");
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_seleniumHost")).clear();
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_seleniumHost")).sendKeys("saucelabs.com");
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_seleniumPort")).clear();
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_seleniumPort")).sendKeys("4444");
+        selenium.findElement(By.id("sauceOnDemandConfigurationForm_save")).click();
+//        verifyTrue(selenium.isTextPresent("Configuration Updated"));
+
 //        verifyEquals("admin", selenium.getValue("sauceOnDemandConfigurationForm_username"));
 //        verifyEquals("saucelabs.com", selenium.getValue("sauceOnDemandConfigurationForm_seleniumHost"));
-//        verifyEquals("4445", selenium.getValue("sauceOnDemandConfigurationForm_seleniumPort"));
-        selenium.click("sub-tree-item-26-link");
-        selenium.click("sub-tree-item-75-unread-count");
-        selenium.click("//div[@id='entries']/div[1]/div/div[1]/div/div[1]/div[5]/div/div/div");
-        selenium.click("//span[@id='sub-tree-item-81-name']/span[1]");
-        selenium.click("sub-tree-item-51-link");
-        selenium.click("//div[@id='entries']/div[1]/div/div[1]/div/div[1]/div[5]/div/div/div");
-        selenium.click("//span[@id='sub-tree-item-81-name']/span[1]");
-        selenium.click("//span[@id='sub-tree-item-2-name']/span[1]");
-        selenium.click("//span[@id='sub-tree-item-81-name']/span[1]");
+//        verifyEquals("4445", selenium.getValue("sauceOnDemandConfigurationForm_seleniumPort"));        
     }
 
 }
--- src/test/java/it/com/saucelabs/bamboo/sod/IntegrationTeztHelper.java
+++ src/test/java/it/com/saucelabs/bamboo/sod/IntegrationTeztHelper.java
@@ -1,34 +1,22 @@
 package it.com.saucelabs.bamboo.sod;
 
 
-import com.atlassian.bamboo.v2.build.BuildContext;
-import com.saucelabs.bamboo.sod.action.BuildConfigurator;
-
-import com.saucelabs.bamboo.sod.util.BambooSauceFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
 import com.saucelabs.ci.sauceconnect.SauceConnectTwoManager;
 import com.saucelabs.ci.sauceconnect.SauceTunnelManager;
 import com.saucelabs.rest.Credential;
 import com.saucelabs.selenium.client.factory.SeleniumFactory;
+import com.thoughtworks.selenium.SeleneseTestCase;
 import com.thoughtworks.selenium.Selenium;
-import org.junit.AfterClass;
-import org.junit.Before;
+import org.apache.commons.lang.StringUtils;
 import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
 
-import org.junit.BeforeClass;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.io.File;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -39,11 +27,20 @@ public class IntegrationTeztHelper {
     protected static final String DEFAULT_SAUCE_DRIVER = "sauce-ondemand:?max-duration=60&os=windows 2008&browser=firefox&browser-version=4.";
     private static final String DUMMY_PLAN_KEY = "test";
 
-    protected Selenium selenium;
+    protected WebDriver selenium;
     private SauceTunnelManager sauceTunnelManager;
 
     @Before
     public void setUp() throws Exception {
+        File sauceSettings = new File(new File(System.getProperty("user.home")), ".sauce-ondemand");
+        if (!sauceSettings.exists()) {
+            String userName = System.getProperty("sauce.user");
+            String accessKey = System.getProperty("access.key");
+            if (StringUtils.isNotBlank(userName) && StringUtils.isNotBlank(accessKey)) {
+                Credential credential = new Credential(userName, accessKey);
+                credential.saveTo(sauceSettings);
+            }
+        }
         final Credential c = new Credential();
         Authenticator.setDefault(
                 new Authenticator() {
@@ -62,30 +59,17 @@ public class IntegrationTeztHelper {
             System.setProperty("SELENIUM_DRIVER", DEFAULT_SAUCE_DRIVER);
         }
 
-        String originalUrl = System.getenv("SELENIUM_STARTING_URL");
-        System.setProperty("SELENIUM_STARTING_URL", "http://localhost:8080/");
+        System.setProperty("SELENIUM_STARTING_URL", "http://localhost:8080/bamboo/");
+        selenium = SeleniumFactory.createWebDriver();
+        selenium.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-
-        selenium = SeleniumFactory.create();
-
-        //selenium = new DefaultSelenium("ondemand.saucelabs.com",
-        //        80, generateStartCommand(properties), "http://localhost:8085/");
-        //selenium.start();
     }
 
 
     @After
     public void tearDown() throws Exception {
-        selenium.stop();
+        selenium.close();
         sauceTunnelManager.closeTunnelsForPlan(DUMMY_PLAN_KEY);
     }
 
-    private String generateStartCommand(Properties properties) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("{\"username\": \"").append(properties.get("sauce.username"));
-        builder.append(",\"access-key\": \"").append(properties.get("sauce.accessKey"));
-        builder.append(",\"os\": \"Windows 2003\"");
-        builder.append("\"browser\": \"firefox\"," + "\"browser-version\": \"3.6.\"," + "\"name\": \"This is an example test\"}");
-        return builder.toString();
-    }
 }
