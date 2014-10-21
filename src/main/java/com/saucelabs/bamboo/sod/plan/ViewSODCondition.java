abe21738a67fe64e50a3efba9274eb1000356ed9
Ross Rowe <piaross@gmail.com>
2014-10-21 21:13:13 +1100

    Update ViewSODCondition to always return true, fixes #18

--- src/main/java/com/saucelabs/bamboo/sod/plan/ViewSODCondition.java
+++ src/main/java/com/saucelabs/bamboo/sod/plan/ViewSODCondition.java
@@ -1,15 +1,8 @@
 package com.saucelabs.bamboo.sod.plan;
 
-import com.atlassian.bamboo.build.Job;
-import com.atlassian.bamboo.plan.Plan;
-import com.atlassian.bamboo.plan.PlanKeys;
-import com.atlassian.bamboo.plan.PlanManager;
-import com.atlassian.bamboo.plan.cache.CachedPlanManager;
 import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.web.Condition;
-import com.saucelabs.bamboo.sod.config.SODMappedBuildConfiguration;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,15 +13,8 @@ import java.util.Map;
  */
 public class ViewSODCondition implements Condition {
 
-    private Map<String, String> map;
-
-    private PlanManager planManager;
-
-    private CachedPlanManager cachedPlanManager;
-
-
     public void init(Map<String, String> map) throws PluginParseException {
-        this.map = map;
+
     }
 
     /**
@@ -39,29 +25,6 @@ public class ViewSODCondition implements Condition {
      */
     public boolean shouldDisplay(Map<String, Object> planMap) {
 
-        boolean result = false;
-        Plan plan = planManager.getPlanByKey(PlanKeys.getPlanKey((String) planMap.get("planKey")));
-
-        if (plan != null) {
-            List<Job> jobs = planManager.getPlansByProject(plan.getProject(), Job.class);
-            for (Job job : jobs) {
-                final SODMappedBuildConfiguration config = new SODMappedBuildConfiguration(job.getBuildDefinition().getCustomConfiguration());
-                if (config.isEnabled()) {
-                    result = true;
-                    break;
-                }
-            }
-        }
-
-        return result;
-
-    }
-
-    public void setPlanManager(PlanManager planManager) {
-        this.planManager = planManager;
-    }
-
-    public void setCachedPlanManager(CachedPlanManager cachedPlanManager) {
-        this.cachedPlanManager = cachedPlanManager;
+        return true;
     }
 }
