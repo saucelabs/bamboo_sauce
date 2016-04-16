b0b1efea11b3750b267c080d6bf4000a2e3a946b
Gavin Mogan <gavin@saucelabs.com>
2016-04-15 17:07:12 -0700

    report proper version of bamboo plugin

--- src/main/java/com/saucelabs/bamboo/sod/StartupHandler.java
+++ src/main/java/com/saucelabs/bamboo/sod/StartupHandler.java
@@ -1,6 +1,5 @@
 package com.saucelabs.bamboo.sod;
 
-import com.atlassian.bamboo.util.*;
 import com.atlassian.sal.api.lifecycle.LifecycleAware;
 import com.saucelabs.saucerest.SauceREST;
 
@@ -8,7 +7,7 @@ public class StartupHandler implements LifecycleAware {
     @Override
     public void onStart() {
         String ver = "Bamboo/" + com.atlassian.bamboo.util.BuildUtils.getCurrentVersion() + " " +
-            "BambooSauceOnDemand/" + com.atlassian.bamboo.util.BuildUtils.getCurrentVersion();
+            "BambooSauceOnDemand/" + com.saucelabs.bamboo.sod.BuildUtils.getCurrentVersion();
         SauceREST.setExtraUserAgent(ver);
     }
 
