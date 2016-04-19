fcae6818ead29ec9b2e1df3e35e9d0bcc70f71e1
Gavin Mogan <github@gavinmogan.com>
2016-04-19 12:57:32 -0700

    Remove 'SauceJobs' tab on jobs that are not enabled (#39)
    * ECO-1000 - Remove 'SauceJobs' tab on jobs which don't have the Sauce Labs/Bamboo plugin enabled - On any error, fall back to default true behavior

--- src/main/java/com/saucelabs/bamboo/sod/plan/ViewSODCondition.java
+++ src/main/java/com/saucelabs/bamboo/sod/plan/ViewSODCondition.java
@@ -1,8 +1,15 @@
 package com.saucelabs.bamboo.sod.plan;
 
+import com.atlassian.bamboo.build.Job;
+import com.atlassian.bamboo.plan.Plan;
+import com.atlassian.bamboo.plan.PlanKeys;
+import com.atlassian.bamboo.plan.PlanManager;
+import com.atlassian.bamboo.v2.build.BuildContext;
 import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.web.Condition;
+import com.saucelabs.bamboo.sod.config.SODMappedBuildConfiguration;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,6 +19,11 @@ import java.util.Map;
  * @author Ross Rowe
  */
 public class ViewSODCondition implements Condition {
+    private PlanManager planManager;
+
+    public void setPlanManager(PlanManager planManager) {
+        this.planManager = planManager;
+    }
 
     /**
      * {@inheritDoc}
@@ -27,6 +39,18 @@ public class ViewSODCondition implements Condition {
      */
     @Override
     public boolean shouldDisplay(Map<String, Object> context) {
-        return true;
+        if (!context.containsKey("planKey") || !context.containsKey("buildKey")) { return true; }
+        Plan plan = planManager.getPlanByKey(PlanKeys.getPlanKey(context.get("planKey").toString()));
+        if (plan == null) { return true; }
+        List<Job> jobs = planManager.getPlansByProject(plan.getProject(), Job.class);
+        for (Job job : jobs) {
+            if (job.getKey().startsWith(context.get("buildKey").toString())) {
+                final SODMappedBuildConfiguration config = new SODMappedBuildConfiguration(job.getBuildDefinition().getCustomConfiguration());
+                if (config.isEnabled()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
