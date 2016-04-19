fac1d0dac0d85f377bcf04a252605c1e8aa5c586
Gavin Mogan <gavin@saucelabs.com>
2016-04-18 17:36:09 -0700

    ECO-1000 - Remove 'SauceJobs' tab on jobs which don't have the Sauce Labs/Bamboo plugin enabled - On any error, fall back to default true behavior

--- src/main/java/com/saucelabs/bamboo/sod/plan/ViewSODCondition.java
+++ src/main/java/com/saucelabs/bamboo/sod/plan/ViewSODCondition.java
@@ -1,7 +1,12 @@
 package com.saucelabs.bamboo.sod.plan;
 
+import com.atlassian.bamboo.plan.Plan;
+import com.atlassian.bamboo.plan.PlanKeys;
+import com.atlassian.bamboo.plan.PlanManager;
+import com.atlassian.bamboo.v2.build.BuildContext;
 import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.web.Condition;
+import com.saucelabs.bamboo.sod.config.SODMappedBuildConfiguration;
 
 import java.util.Map;
 
@@ -12,6 +17,16 @@ import java.util.Map;
  * @author Ross Rowe
  */
 public class ViewSODCondition implements Condition {
+    protected PlanManager planManager;
+
+    public PlanManager getPlanManager() {
+        return planManager;
+    }
+
+    public void setPlanManager(PlanManager planManager) {
+        this.planManager = planManager;
+    }
+
 
     /**
      * {@inheritDoc}
@@ -27,6 +42,13 @@ public class ViewSODCondition implements Condition {
      */
     @Override
     public boolean shouldDisplay(Map<String, Object> context) {
-        return true;
+        if (!context.containsValue("buildKey")) { return true; }
+        Plan plan = planManager.getPlanByKey(PlanKeys.getPlanKey(context.get("buildKey").toString()));
+        if (plan == null) { return true; }
+        SODMappedBuildConfiguration config = new SODMappedBuildConfiguration(
+            plan.getBuildDefinition().getCustomConfiguration()
+        );
+        if (config == null) { return true; }
+        return config.isEnabled();
     }
 }
